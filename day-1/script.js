63cacf80582b4b57571e2fc0882fcdbfb98ae893
AREGE.SRIKALA <aregesrikala@gmail.com>
2026-05-13 11:23:32 +0530

    files are added

---
+++ day-1/script.js
@@ -0,0 +1,22 @@
+console.log("hello world")
+var name = "srikala"
+var num = 10
+console.log(name)
+var num1 = 20
+if (num<num1){
+    console.log(num +" is less than the " + num1)
+}else{
+    console.log(num +" is greater than the "+ num1)
+}
+var num2 = 25
+if (num2%2==0){
+    console.log(num2+" is a even number.")
+}else{
+    console.log(num2+" is not an even number.")
+}
+var num3 = 2004
+if (num3%4==0){
+    console.log(num3+" is a leap year.")
+}else{
+    console.log(num3+" is not a leap year.")
+}
